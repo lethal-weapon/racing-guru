1f7a032088d0e25f28d320b3bb29b4c397685ee1
Sean Wong <sean.wong@agileexlab.net>
2022-10-10 23:20:58 +0800

    Enhance racecard grid

--- src/app/racecard/racecard.component.ts
+++ src/app/racecard/racecard.component.ts
@@ -23,6 +23,43 @@ export class RacecardComponent implements OnInit {
     }
   }
 
+  hideBottomBorder(jockey: string, raceNum: number): boolean {
+    return !(
+      raceNum === 5
+      || raceNum === 10
+      || this.rideThisRace(jockey, raceNum)
+      || this.rideNextRace(jockey, raceNum)
+    )
+  }
+
+  hideRightBorder(jockey: string, raceNum: number): boolean {
+    return !(
+      jockey === this.jockeys.pop()
+      || this.isBoundaryJockey(jockey)
+      || this.rideThisRace(jockey, raceNum)
+      || this.rideThisRace(this.jockeys[this.jockeys.indexOf(jockey) + 1], raceNum)
+    )
+  }
+
+  rideThisRace(jockey: string, raceNum: number): boolean {
+    return (jockey[0] < 'G' && raceNum % 2 == 0) || (jockey[0] > 'J' && raceNum % 2 == 1)
+  }
+
+  rideNextRace(jockey: string, raceNum: number): boolean {
+    return raceNum < 10 && this.rideThisRace(jockey, raceNum + 1)
+  }
+
+  isBoundaryJockey(jockey: string): boolean {
+    return this.meetingBoundaryJockeys.includes(jockey)
+  }
+
+  getTrainer(jockey: string, raceNum: number): string {
+    return [
+      'SJJ', 'LFC', 'CAS', 'WDJ', 'YPF', 'LKW', 'FC', 'SCS', 'YTP', 'HDA', 'HAD',
+      'SWY', 'TKH', 'MKL', 'YCH', 'NPC', 'RW', 'MA', 'GR', 'CCW', 'TYS', 'HL'
+    ][(this.jockeys.indexOf(jockey) * raceNum) % 20 + 1]
+  }
+
   get meetings(): string[] {
     return [
       'SUN, 2022-10-09, ST, 10 Races, 19 Jockeys, 22 Trainers, 123 Horses',
@@ -31,15 +68,19 @@ export class RacecardComponent implements OnInit {
     ]
   }
 
+  get meetingBoundaryJockeys(): string[] {
+    return ['BA', 'LDE', 'BV']
+  }
+
   get jockeys(): string[] {
     return [
-      'PZ', 'TEK', 'DSS', 'BA', 'HCY', 'CML', 'PMF', 'LDE',
-      'MMR', 'FEL', 'HEL', 'CLR', 'BHW', 'HAA', 'BV',
-      'CJE', 'CCY', 'YML', 'MHT', 'LHW',
+      'PZ', 'TEK', 'DSS', 'BA', 'CML', 'PMF', 'LDE',
+      'MMR', 'FEL', 'CLR', 'BHW', 'BV',
+      'CJE', 'CCY', 'YML', 'MHT', 'LHW', 'CHA', 'WJH', 'WCV'
     ]
   }
 
-  get races(): {raceNum: number, raceClass: string, distance:number, track: string}[] {
+  get races(): { raceNum: number, raceClass: string, distance: number, track: string }[] {
     return [
       {
         raceNum: 1,
