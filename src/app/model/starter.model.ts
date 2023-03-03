c88c775e8e2e841de82c230ce7cd83c4c91a07e1
Sean Wong <sean.wong@agileexlab.net>
2023-03-04 01:23:30 +0800

    Add more field in Starter

--- src/app/model/starter.model.ts
+++ src/app/model/starter.model.ts
@@ -1,13 +1,20 @@
 export class Starter {
   constructor(
-    public order       : number,
-    public draw        : number,
-    public horse       : string,
-    public jockey      : string,
-    public trainer     : string,
-    public horseNameEN : string,
-    public horseNameCH : string,
-    public bonusRun    : boolean
+    public order          : number,
+    public draw           : number,
+    public horse          : string,
+    public jockey         : string,
+    public trainer        : string,
+    public horseNameEN    : string,
+    public horseNameCH    : string,
+    public ownerCH        : string,
+    public origin         : string,
+    public age            : number,
+    public horseTotal1st  : number,
+    public horseTotal2nd  : number,
+    public horseTotal3rd  : number,
+    public horseTotalRuns : number,
+    public bonusRun       : boolean
   ) {
   }
 }
