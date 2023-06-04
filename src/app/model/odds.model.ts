a91abb53a2500d128e9a839393a4b6fd5cd0f704
Sean Kramer <sean@kramerdesigns.app>
2023-06-04 21:10:22 +0800

    Add tierce investment field in Odds

--- src/app/model/odds.model.ts
+++ src/app/model/odds.model.ts
@@ -1,9 +1,3 @@
-export interface WinPlaceOdds {
-  order : number,
-  win   : number,
-  place : number
-}
-
 export interface Singular {
   odds  : number,
   order : number
@@ -14,12 +8,26 @@ export interface Combination {
   orders : number[]
 }
 
+export interface WinPlaceOdds {
+  order : number,
+  win   : number,
+  place : number
+}
+
+export interface TierceInvestment {
+  order  : number,
+  win    : number,
+  second : number,
+  third  : number
+}
+
 export class Odds {
   constructor(
     public winPlace       : WinPlaceOdds[],
     public quinella       : Combination[],
     public forecast       : Combination[],
     public trio           : Combination[],
+    public tierce         : TierceInvestment[],
     public firstFour     ?: Combination[],
     public quinellaPlace ?: Combination[],
     public double        ?: Combination[]
