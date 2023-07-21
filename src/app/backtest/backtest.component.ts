36c49c37a19b2c79b60e9b4a0c4ec2dd520eed24
Sean Wong <wongse@oocl.com>
2023-07-21 18:10:00 +0800

    Add meeting table in Backtest page

--- src/app/backtest/backtest.component.ts
+++ src/app/backtest/backtest.component.ts
@@ -1,13 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 
 import {RestRepository} from '../model/rest.repository';
-import {TesterYield} from '../model/backtest.model';
+import {MeetingYield, TesterYield} from '../model/backtest.model';
 
 @Component({
   selector: 'app-backtest',
   templateUrl: './backtest.component.html'
 })
 export class BacktestComponent implements OnInit {
+  activeVersion = '';
 
   constructor(private repo: RestRepository) {
   }
@@ -16,8 +17,18 @@ export class BacktestComponent implements OnInit {
     this.repo.fetchYields();
   }
 
-  getReturnOnInvestment = (tyield: TesterYield): number =>
+  setActiveVersion = (clicked: string) =>
+    this.activeVersion = this.activeVersion === clicked ? '' : clicked
+
+  getReturnOnInvestment = (tyield: TesterYield | MeetingYield): number =>
     parseFloat((tyield.credit / tyield.debit - 1).toFixed(2))
+
+  countRacesOnMeeting = (myield: MeetingYield): number[] => {
+    const total = myield.races.length;
+    const betRaces = myield.races.filter(r => r.debit > 0);
+    const positive = betRaces.filter(r => r.credit > r.debit);
+    return [positive.length, betRaces.length, total];
+  }
 
   countMeetings = (tyield: TesterYield): number[] => {
     const total = tyield.meetings.length;
@@ -45,6 +56,12 @@ export class BacktestComponent implements OnInit {
     return [positive, betlines.length];
   }
 
+  get meetingFields(): string[] {
+    return this.fields
+      .filter((f, index) => index > 0)
+      .map(f => f === 'Meetings' ? 'Meeting' : f);
+  }
+
   get fields(): string[] {
     return [
       'Tester',
@@ -56,6 +73,12 @@ export class BacktestComponent implements OnInit {
       'Profit / Loss',
       'ROI'
     ];
+  }
+
+  get meetingYields(): MeetingYield[] {
+    return this.yields
+      .find(ty => ty.version === this.activeVersion)
+      ?.meetings || [];
   }
 
   get yields(): TesterYield[] {
