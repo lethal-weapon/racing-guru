587e3249ee7fb6bc8874f0bba05360e774042703
Sean Kramer <sean@kramerdesigns.app>
2024-08-31 13:43:39 +0800

    Add stats section in trend-trackwork page

--- src/app/trend-trackwork/trend-trackwork.component.ts
+++ src/app/trend-trackwork/trend-trackwork.component.ts
@@ -1,9 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 
 import {RestRepository} from '../model/rest.repository';
+import {Player} from '../model/player.model';
 import {TrackworkSnapshot, TrackworkStarter} from '../model/trackwork.model';
 import {formatMeeting, toPlacingColor} from '../util/functions';
 import {MAX_RACE_PER_MEETING} from '../util/numbers';
+import {
+  ODDS_INTENSITIES,
+  OddsIntensity,
+  PLACING_MAPS,
+  RATING_GRADES
+} from '../util/strings';
+
+const BY_STATS = 'By Stats';
 
 @Component({
   selector: 'app-trend-trackwork',
@@ -11,8 +20,12 @@ import {MAX_RACE_PER_MEETING} from '../util/numbers';
 })
 export class TrendTrackworkComponent implements OnInit {
 
-  activeMeeting: string = '';
+  activeBadge: string = BY_STATS;
 
+  protected readonly BY_STATS = BY_STATS;
+  protected readonly PLACING_MAPS = PLACING_MAPS;
+  protected readonly RATING_GRADES = RATING_GRADES;
+  protected readonly ODDS_INTENSITIES = ODDS_INTENSITIES;
   protected readonly formatMeeting = formatMeeting;
   protected readonly toPlacingColor = toPlacingColor;
 
@@ -20,23 +33,47 @@ export class TrendTrackworkComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.repo.fetchTrackworkSnapshots(8, () => {
-      this.activeMeeting = this.repo.findTrackworkSnapshots()
-        .map(t => t.meeting)
-        .sort((m1, m2) => m2.localeCompare(m1))
-        .shift() || '';
-    });
+    this.repo.fetchTrackworkSnapshots(8);
   }
 
-  getBadgeStyle = (renderMeeting: string): string =>
-    this.activeMeeting === renderMeeting
+  getBadgeStyle = (render: string): string =>
+    this.activeBadge === render
       ? `text-yellow-400 border-yellow-400`
       : `border-gray-600 hover:border-yellow-400 hvr-float-shadow cursor-pointer`
 
-  getRaceStarter = (race: number): TrackworkStarter[] =>
+  getGradePlacingStartersInOddsRange = (
+    grade: string,
+    placing: number,
+    range: OddsIntensity
+  ): TrackworkStarter[] =>
+    this.getGradePlacingStarters(grade, placing)
+      .filter(s => s.winOdds >= range.lower && s.winOdds <= range.upper)
+
+  getGradePlacingStarters = (grade: string, placing: number): TrackworkStarter[] =>
+    this.getGradeStarters(grade).filter(s => s.placing === placing)
+
+  getGradeTop4Starters = (grade: string): TrackworkStarter[] =>
+    this.getGradeStarters(grade).filter(s => s.placing >= 1 && s.placing <= 4)
+
+  getGradeStarters = (grade: string): TrackworkStarter[] =>
+    this.starters.filter(s => s.grade === grade)
+
+  getRaceStarters = (race: number): TrackworkStarter[] =>
     this.activeTrackwork.starters
       .filter(s => s.race === race)
       .sort((s1, s2) => (s2.intensity - s1.intensity) || (s1.order - s2.order))
+
+  getTrainerGradeTop4Starters = (trainer: Player, grade: string): TrackworkStarter[] =>
+    this.getTrainerTop4Starters(trainer).filter(s => s.grade === grade)
+
+  getTrainerTop4Starters = (trainer: Player): TrackworkStarter[] =>
+    this.getTrainerStarters(trainer).filter(s => s.placing >= 1 && s.placing <= 4)
+
+  getTrainerStarters = (trainer: Player): TrackworkStarter[] =>
+    this.starters.filter(s => s.trainer === trainer.code)
+
+  getTrainerGroupStarters = (trainers: Player[]): TrackworkStarter[] =>
+    this.starters.filter(s => trainers.map(t => t.code).includes(s.trainer))
 
   getGradeBorderStyle = (
     race: number,
@@ -46,7 +83,7 @@ export class TrendTrackworkComponent implements OnInit {
 
     if (starter.grade !== 'A') return '';
 
-    const sortedStarters = this.getRaceStarter(race);
+    const sortedStarters = this.getRaceStarters(race);
     if (index === sortedStarters.length - 1) return '';
 
     return sortedStarters[index + 1]?.grade === 'B'
@@ -61,8 +98,14 @@ export class TrendTrackworkComponent implements OnInit {
       .pop() || MAX_RACE_PER_MEETING;
   }
 
+  get starters(): TrackworkStarter[] {
+    return this.trackworks
+      .flatMap(ts => ts.starters)
+      .filter(s => !s.scratched && s?.placing)
+  }
+
   get activeTrackwork(): TrackworkSnapshot {
-    const match = this.trackworks.find(ts => ts.meeting === this.activeMeeting);
+    const match = this.trackworks.find(ts => ts.meeting === this.activeBadge);
     return match ? match : this.trackworks[0];
   }
 
@@ -70,7 +113,34 @@ export class TrendTrackworkComponent implements OnInit {
     return this.repo.findTrackworkSnapshots();
   }
 
+  get trainerGroups(): Player[][] {
+    const boundaryTrainers = this.trainers.filter(t => t.boundary);
+    const group = this.trainers
+      .filter(t => t.boundary)
+      .map((bt, bti) => {
+        let startIndex = 0;
+        let endIndex = this.trainers.findIndex(p => p === bt);
+
+        if (bti > 0) {
+          startIndex = 1 + this.trainers.findIndex(p => p === boundaryTrainers[bti - 1]);
+        }
+
+        return this.trainers.slice(startIndex, endIndex + 1);
+      });
+
+    const lastStartIndex =
+      1 + this.trainers.findIndex(p => p === boundaryTrainers[boundaryTrainers.length - 1]);
+
+    group.push(this.trainers.slice(lastStartIndex));
+    return group;
+  }
+
+  get trainers(): Player[] {
+    return this.repo.findPlayers().filter(p => !p.jockey);
+  }
+
   get isLoading(): boolean {
-    return this.repo.findTrackworkSnapshots().length === 0;
+    return this.repo.findPlayers().length === 0
+      || this.repo.findTrackworkSnapshots().length < 2;
   }
 }
